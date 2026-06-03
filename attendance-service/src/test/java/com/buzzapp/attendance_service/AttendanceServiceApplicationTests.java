8aa7407fadacc1d9068c2dd80e0e10f4dbf51f4d
Odeneho Affram-Mensah <odenehoafframmensah36@gmail.com>
2026-06-03 12:09:29 +0000

    Add auth, attendance, and safety service scaffolds

---
+++ attendance-service/src/test/java/com/buzzapp/attendance_service/AttendanceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buzzapp.attendance_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
