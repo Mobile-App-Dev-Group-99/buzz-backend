2240a83ac03082524e5b8dd7238c6d5856567506
Odeneho Affram-Mensah <odenehoafframmensah36@gmail.com>
2026-06-05 17:00:57 +0000

    Added User entity, Role enum, login DTOs, and UserRepository

---
+++ auth-service/src/main/java/com/buzzapp/auth_service/model/User.java
@@ -0,0 +1,26 @@
+package com.buzzapp.auth_service.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true, length = 255)
+    private  String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
